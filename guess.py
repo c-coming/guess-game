ae35bb9b989f51fabcf13f8783f61afa36cb1395
c-coming <c-coming@163.com>
2018-05-14 17:17:26 +0800

    add 5time

--- guess.py
+++ guess.py
@@ -1,14 +1,23 @@
-from random import randint
+# -*- coding: utf-8 -*-
+from random import randint #导入模块
 num = randint(1,10)
-print num
+print "Num:",num
+
 def main():
     print "Welcome to the number guessing game!"
+    i = 0
     while True:
-        guess = raw_input("Please guess a number between 1 to 10: ")
-        if int(guess) == num:
-            print "This is right!"
-            break
+        i += 1
+        if i <= 5:
+            print "Time:",i
+            guess = raw_input("Please guess a number between 1 to 10: ")
+            if int(guess) == num:
+                print "This is right!"
+                break
+            else:
+                print "I'm sorry, you're wrong."
         else:
-            print "I'm sorry,Guess again!"
+            print "Game Over!"
+            break
 if __name__ == "__main__":
     main()
