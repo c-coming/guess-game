f87f543ac7208d8780de39b96531cde423ca9a68
c-coming <c-coming@163.com>
2018-05-14 17:22:53 +0800

    fix a bug

--- guess.py
+++ guess.py
@@ -4,11 +4,14 @@ print num
 def main():
     print "Welcome to the number guessing game!"
     while True:
-        guess = raw_input("Please guess a number between 1 to 10: ")
-        if int(guess) == num:
-            print "This is right!"
-            break
-        else:
-            print "I'm sorry,Guess again!"
+        try:
+            guess = raw_input("Please guess a number between 1 to 10: ")
+            if int(guess) == num:
+                print "This is right!"
+                break
+            else:
+                print "I'm sorry,Guess again!"
+        except:
+            print "Please enter a number!"
 if __name__ == "__main__":
     main()
