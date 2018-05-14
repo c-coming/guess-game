87c926268c243d92047f360f89b070fb43203039
c-coming <c-coming@163.com>
2018-05-14 17:52:02 +0800

    fix a bug and add 5time

--- guess.py
+++ guess.py
@@ -4,14 +4,20 @@ print num
 def main():
     print "Welcome to the number guessing game!"
     while True:
-        try:
-            guess = raw_input("Please guess a number between 1 to 10: ")
-            if int(guess) == num:
-                print "This is right!"
-                break
-            else:
+        i += 1
+        if i <= 5:
+            print "Time:",i
+            try:
+                guess = raw_input("Please guess a number between 1 to 10: ")
+                if int(guess) == num:
+                    print "This is right!"
+                    break
+                else:
+                    print "I'm sorry, you're wrong."
+            except:
                 print "I'm sorry,Guess again!"
-        except:
-            print "Please enter a number!"
+        else:
+            print "Game Over!"
+break
 if __name__ == "__main__":
     main()
